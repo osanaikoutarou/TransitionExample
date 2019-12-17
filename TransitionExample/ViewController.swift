783b88bf60eeeb4b13d882b6ee166047582aa99c
osanai <osanai.koutarou@mdpr.co.jp>
2019-12-17 12:28:15 +0900

    Initial Commit

---
+++ TransitionExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TransitionExample
+//
+//  Created by 長内幸太郎 on 2019/12/17.
+//  Copyright © 2019 長内幸太郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
