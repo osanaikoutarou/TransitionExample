a4d5c61867a797ccd044d533d71fdb157b059870
osanai <osanai.koutarou@mdpr.co.jp>
2019-12-17 19:16:16 +0900

    first commit

---
+++ TransitionExample/NextViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NextViewController.swift
+//  TransitionExample
+//
+//  Created by 長内幸太郎 on 2019/12/17.
+//  Copyright © 2019 長内幸太郎. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TransitionExample/RedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RedViewController.swift
+//  TransitionExample
+//
+//  Created by 長内幸太郎 on 2019/12/17.
+//  Copyright © 2019 長内幸太郎. All rights reserved.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
